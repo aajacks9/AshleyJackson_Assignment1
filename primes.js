85c81ac490a037afbda4dc047a18c86ba8e145b2
aajacks9 <aajacks@gmail.com>
2018-01-18 15:32:05 -0600

    Comment Explaination
    I typed out all the parts that I need in order to answer the question. Now I need to create the functions.

---
+++ primes.js
@@ -0,0 +1,12 @@
+/* I need to first have a function that determines prime number
+    then I need to have a function that stops finding prime numbers after the 1000th
+ */
+
+/* prime number function*/
+
+I need a counter that only counts the prime numbers
+I also need a function that deterimes if a number is prime or not
+I also need a function that stops the counter after it reaches the 1000th prime number
+so i need to go through each number determine if it is prime, if so then add it to the count
+then start the function again until the count is 1000
+then display that final number that was counted 
