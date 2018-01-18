db58c9488b81eaeead5f8f4d861711a5b7e5b683
aajacks9 <aajacks@gmail.com>
2018-01-18 16:15:16 -0600

    Struggling with Primes Problem
    I've written out what I need, but I'm struggling to find the exact tags and wording to accomplish the steps I know are needed.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -20,15 +20,29 @@ for (var i = 1; i <= 100; i++ )
 
 
 
-/* I need to first have a function that determines prime number
-    then I need to have a function that stops finding prime numbers after the 1000th
- */
-
-/* prime number function*/
-
-I need a counter that only counts the prime numbers
-I also need a function that deterimes if a number is prime or not
+/*I need a counter that only counts the prime numbers
+I also need a function that determines if a number is prime or not
 I also need a function that stops the counter after it reaches the 1000th prime number
 so i need to go through each number determine if it is prime, if so then add it to the count
 then start the function again until the count is 1000
-then display that final number that was counted
+then display that final number that was counted*/
+
+
+var c = 0
+function IsPrime(value) {
+
+    for (var i = 2; i < value; i++) {
+        var remainder = value % i;
+        if (remainder === 0)
+            return false;
+        if (value > 1)
+            (c++);
+    }
+
+    if c === 1000
+        return value
+}
+
+
+
+
